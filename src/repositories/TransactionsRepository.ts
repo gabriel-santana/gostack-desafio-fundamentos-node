7dfdc70675a0a3ddcbc87fddbca19e45782ee871
Gabriel Santana <gabrielsantana77@hotmail.com>
2020-05-17 17:12:36 -0300

    finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,54 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  private balance: Balance = {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  };
 
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'income') {
+      this.balance.income += value;
+    } else if (type === 'outcome') {
+      if (value > this.balance.total) {
+        throw Error(
+          'Cannot create outcome transaction without a valid balance',
+        );
+      } else {
+        this.balance.outcome += value;
+      }
+    } else {
+      throw Error('Cannot create transaction without a valid type');
+    }
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
